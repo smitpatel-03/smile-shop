299dcc5a7a41875f7a57fe5f954e738bae82ea7b
smitpatel-03 <patelsmitkumar03@gmail.com>
2022-05-24 22:53:09 +0530

    connect mongodb

---
+++ backend/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewurlParser: true, //both already in-built supported in mongodb but i added only!
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`database is connnected to host: ${data.connection.host}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const app = require("./app");
-
+const connectDatabase = require("./config/database");
 const dotenv = require("dotenv");
 
 dotenv.config({
   path: "backend/config/config.env",
 });
 
+connectDatabase();
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
