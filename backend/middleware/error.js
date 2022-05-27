272b778712fb7ecfeaaf9a05a04d283cdf4d49a7
smitpatel-03 <patelsmitkumar03@gmail.com>
2022-05-27 23:26:29 +0530

    jwt-error-handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,23 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 404);
   }
 
+  //Mogoose duplicate key error
+
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  if (err.name === "JsonwebTokenError") {
+    const message = `Json web token error, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  if (err.name === "JsonwebExpireError") {
+    const message = `Json web token is Expired, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
